4e39120c03e2fd7e312aed46407fd03f15913111
Brandon O'Neal <baoneal95@gmail.com>
2020-10-16 19:05:18 -0700

    installed cra and set up file structure

---
+++ water-my-plants/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Water My Plants</h1>
+    </div>
+  );
+}
+
+export default App;
