7ee18ea0447e7028871ead99a9541deae51e2d3e
Brandon O'Neal <baoneal95@gmail.com>
2020-10-17 17:44:45 -0700

    built basic plant form and setup axioswithauth and privateroute

---
+++ water-my-plants/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/App.css";
+import MyPlants from "./components/main-app/MyPlants";
+
+const App = () => {
+  return (
+    <div className="App">
+      <MyPlants />
+    </div>
+  );
+};
+
+export default App;
